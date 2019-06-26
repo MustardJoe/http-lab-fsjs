e0fe66229e08e7a94d6c3c78a9b3f5765a8cbf41
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 15:40:57 -0700

    added some stuff into the test

--- app.test.js
+++ app.test.js
@@ -1,13 +1,24 @@
 const request = require('supertest');
 const app = require('./lib/app');
 
-describe('application routes', () => {
-  it('requires a list of dogs', () => {
-    return request(app)
+describe('routing http paths with our server', () => {
+
+  it('shows the welcome page', done => {
+    request(app)
       .get('/')
       .then(res => {
         //res.status & res.status
-        expect(res.text).toEqual(expect.stringContaining('spot'));
+        expect(res.text).toEqual(expect.stringContaining('raptors'));
+        done();
+      });
+  });
+
+  it('has the red route', done => {
+    request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toContain('red');
+        done();
       });
   });
 });
