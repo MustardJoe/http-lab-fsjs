b75fb67012ee62759a67171f32f92cdb14836bf6
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 14:07:33 -0700

    excercise 2 commit

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ const server = createServer((req, res) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('hi');
+  res.end(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <style>
+    body {
+      background-color: pink;
+    }
+  </style>
+</head>
+<body>
+  <h1>here the world ends with raptors and reptilicus</h1>
+  
+</body>
+</html>
+  `);
 });
 
 server.listen(9999, () => {
