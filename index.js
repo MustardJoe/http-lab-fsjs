ed79d8b3aedb4a8e5a12186bd2a1d46f93f93811
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-26 14:51:14 -0700

    moved code from this file app file

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-const { createServer } = require('http');
+const app = require('./lib/app');
 
-const server = createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <style>
-    body {
-      background-color: pink;
-    }
-  </style>
-</head>
-<body>
-  <h1>here the world ends with raptors and reptilicus</h1>
-  <div>npm run start:watch</div>
-</body>
-</html>
-  `);
-});
-
-server.listen(9999, () => {
+app.listen(9999, () => {
   console.log('listening on port 9999');
 });
